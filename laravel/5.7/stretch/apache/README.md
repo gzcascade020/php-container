8fdaf9c889d7a0246d6c35de02ebfd02d72d7597
Jill Z Pan <jill.z.pan@pccw.com>
2020-05-22 17:11:54 +0800

    Add laravel 5.7 apache stretch

---
+++ laravel/5.7/stretch/apache/README.md
@@ -0,0 +1,32 @@
+Laravel 5.7 container image
+================
+
+This container image includes PHP 7.2 as a base image for your Laravel 5.7 applications.
+
+Description
+-----------
+
+Laravel 5.7 available as container is a base platform for
+running various Laravel 5.7 frameworks.
+Laravel is a web application framework with expressive, elegant syntax.
+We’ve already laid the foundation freeing you to create without sweating the small things.
+
+Environment variables
+---------------------
+
+The following environment variables set their equivalent property value in the .env* file:
+* **LARAVEL_CONFIG_CACHE**
+  * Set this to a empty value to skip processing [optimizing configuration](https://laravel.com/docs/5.7/deployment#optimizing-configuration-loading)
+  * Default: 1
+* **LARAVEL_SECRETS**
+  * Set this to a empty value to skip sets the .env values with the [docker secret](https://docs.docker.com/engine/swarm/secrets/#how-docker-manages-secrets) on startup.
+  * Default: 1
+* **LARAVEL_SECRET_{{LARAVEL_ENV_KEY}}**
+  * Set this variable to use the docker secret file content to automatically update the *LARAVEL_ENV_KEY* in *LARAVEL_ENV_FILES* on startup.
+  * Default: 1  
+* **LARAVEL_ENV_EXAMPLE_FILES**
+  * This variable can be used to specify the env example files which will automatically be renamed to .env. Otherwise is contained.
+  * Default: .env.example
+* **LARAVEL_ENV_FILES**
+  * This variable can be used to specify the env files which needs to be replaced with the docker secret is contained.
+  * Default: .env
---
+++ laravel/5.7/stretch/apache/pre-start/30-laravel-config.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+source ${PHP_CONTAINER_SCRIPTS_PATH}/common.sh
+
+log_info 'Processing laravel configuration ...'
+
+LARAVEL_CONFIG_CACHE=${LARAVEL_CONFIG_CACHE:-1}
+LARAVEL_SECRETS=${LARAVEL_SECRETS:-1}
+LARAVEL_ENV_EXAMPLE_FILES=${LARAVEL_ENV_EXAMPLE_FILES:-.env.example}
+LARAVEL_ENV_FILES=${LARAVEL_ENV_FILES:-.env}
+
+# Copy .env*.example files to .env*
+function process_laravel_env_files {
+    local example_files filename
+    IFS=':' read -r -a example_files <<<"${LARAVEL_ENV_EXAMPLE_FILES}"
+    if [ ${#example_files[@]} -ne 0 ]
+    then
+        log_info 'Processing laravel env files ...'
+        for example_file in "${example_files[@]}"
+        do
+            filename="${example_file%.example}"
+            if [ -f "${APP_DATA}/${example_file}" ] && [ ! -f "${APP_DATA}/${filename}" ]
+            then
+                log_and_run cp -p "${APP_DATA}/${example_file}" "${APP_DATA}/${filename}"
+            fi
+        done
+    fi
+}
+
+function get_env_files {
+    local files result
+    IFS=':' read -r -a files <<<"${LARAVEL_ENV_FILES}"
+    for file in "${files[@]}"
+    do
+        [[ -f "${file}" ]] && result="${result} ${file}"
+    done
+    echo ${result}
+}
+
+# Replace env values with the docker secrets
+function process_laravel_docker_secrets {
+    local env_files=$(get_env_files)
+    if [ "${LARAVEL_SECRETS}" -ne 0 ] && [ ! -z "${env_files}" ]
+    then
+        local secret_file target val
+        log_info 'Processing laravel env docker secrets ...'
+        for secret in "${!LARAVEL_SECRET_@}"
+        do
+            secret_file=$(printenv "${secret}")
+            if [ -r "${secret_file}" ]
+            then
+                target="${secret#LARAVEL_SECRET_}"
+                val=$(cat "${secret_file}")
+                log_info "Running sed -i -e \"s#^${target}=.*\$#${target}=******#g\" ${env_files}"
+                sed -i -e "s#^${target}=.*\$#${target}=${val}#g" ${env_files}
+            fi
+        done
+    fi
+}
+
+function artisan_config_cache {
+    if [ "${LARAVEL_CONFIG_CACHE}" -ne 0 ]
+    then
+        log_and_run php artisan config:cache
+    fi    
+}
+
+process_laravel_env_files
+process_laravel_docker_secrets
+artisan_config_cache
